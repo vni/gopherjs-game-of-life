e162319cccec2d7b55be4655a633551a149e73c1
Nikolay Vasylchyshyn <younghead@ukr.net>
2016-06-29 19:35:34 +0300

    Minor changes.
    * using javascript Math.random instead of go math/rand
      (and eliminated use of 2 imports: math/rand and time)
    
    * cleaned the code a little bit

--- GameOfLife.go
+++ GameOfLife.go
@@ -1,14 +1,7 @@
 // TODO LIST:
-// * separate into 2 files, GameOfLife.go and js.go (and, maybe, termbox?)
-// * clean code
-// * git repo
 // * remove/hide global variables
-// * add termbox output support
-// * create Makefile to build js-canvas and termbox
 
 package main
-import "time"
-import "math/rand"
 import "github.com/gopherjs/gopherjs/js"
 
 const (
@@ -16,33 +9,18 @@ const (
 	COLS = 170
 	GENERATIONS = 100
 	CELL_SIZE = 12
-	TICK_LENGTH = 100 // milliseconds
+	TICK_LENGTH = 200 // milliseconds
 	INITIAL_ALIVE_THRESHOLD = 35 // percentage
 )
 
 var g_rows, g_cols int
 
 // GLOBAL VARIABLES
-var Generation int
 var colors []string = []string{"red", "blue", "brown", "yellow", "green", "lightyellow", "orange", "cyan", "pink", "lime"}
 var color string
 var Width, Height int
-
-/*
-type Canvas struct {
-	canvas *js.Object
-	ctx *js.Object
-}
-
-func (c *Canvas) width() int {
-	return c.canvas.Get("width")
-}
-
-func (c *Canvas) height() int {
-	return c.canvas.Get("height")
-}
-*/
-
+var g_math *js.Object
+var g_board Board
 
 // ======================================================================
 // cell
@@ -85,7 +63,7 @@ func NewBoard(r, c int) *Board {
 func (b *Board) RandomlySeedBoard() *Board {
 	for r:=0; r<b.rows; r++ {
 		for c:=0; c<b.cols; c++ {
-			if rand.Intn(100) < INITIAL_ALIVE_THRESHOLD {
+			if random(100) < INITIAL_ALIVE_THRESHOLD {
 				b.board[b.current][r][c].alive = true
 			} else {
 				b.board[b.current][r][c].alive = false
@@ -194,71 +172,44 @@ func createCanvas() *js.Object {
 	body := document.Get("body")
 	body.Get("style").Set("margin", "0px")
 	body.Get("style").Set("padding", "0px")
-	document.Get("body").Call("appendChild", canvas)
-
-	println("width:", width, ", g_cols:", g_cols)
-	println("height:", height, ", g_rows:", g_rows)
+	body.Call("appendChild", canvas)
 
 	// SETUP EVENTS
-	body.Call("addEventListener", "keydown", func() {
-		println("keydown")
-	})
-
-	body.Call("addEventListener", "keyup", func() {
-		println("keyup")
-	})
-
-	body.Call("addEventListener", "keypress", func() {
-		js.Global.Call("clearInterval", func() {
-		})
-		println("keypress")
-	})
-
 	canvas.Call("addEventListener", "click", func() {
-		println("click")
-		color = colors[rand.Intn(len(colors))]
+		color = colors[random(len(colors))]
 	})
 
 	return canvas
 }
 
-/*
-func createTextOutput() *js.Object {
-	document := js.Global.Get("document")
-	p := document.Call("createElement", "p")
-	document.Get("body").Call("appendChild", p)
-	return p
+// ======================================================================
+// auxiliary functions
+// ======================================================================
+
+func random(n int) int {
+	f := g_math.Call("random").Float()
+	f *= float64(n)
+	return int(f)
 }
-*/
+
+func init() {
+	g_math = js.Global.Get("Math")
+}
 
 // ======================================================================
 // main
 // ======================================================================
 
-var g_board Board
-
 func main() {
-	rand.Seed(time.Now().UnixNano())
-
-	color = colors[rand.Intn(len(colors))]
+	color = colors[random(len(colors))]
 
 	canvas := createCanvas()
 	ctx := canvas.Call("getContext", "2d")
 
 	g_board = *NewBoard(g_rows, g_cols)
 	g_board.RandomlySeedBoard()
-	//g_board = *NewRandomBoard(g_rows, g_cols)
-
-	println("Canvas initalized.")
-
-	//println("js.Global.Call(\"Math.random\"):", js.Global.Call("Math.random"))
-	//println("js.Global.Call(\"Math.random\"):", js.Global.Call("Math.random"))
-	//println(js.Global.Get("document").Call("Math.random"))
-	//println(js.Global.Call("random"))
-
 
 	js.Global.Call("setInterval", func(){
-		Generation++
 		g_board.draw(ctx)
 		g_board.step()
 	}, TICK_LENGTH)
